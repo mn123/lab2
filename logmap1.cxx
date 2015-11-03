6ee3c13bf9bf0ccd304a9945dee6846e4ec63141
Matthias Neufeld <maneuf@pc10.pool.tp1.uni-duesseldorf.de>
2015-11-03 15:54:20 +0100

    Aufgabe gelöst

--- logmap1.cxx
+++ logmap1.cxx
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
  int N=100; //Number of iterations
  int n=400; //Number of different values for r
- int m=19;
+ int m=19; //don't plot the first 20 values of x
  double r=0,S=0.01;
  for(int j=1; j<=n; j++){
    r=r+S;
@@ -15,7 +15,7 @@ int main(){
    for(int i=20; i<=N; i++){
     x=x*r*(1-x);
     cout << r <<"\t"<< x << endl;
-  }
+   }
  } 
   return 0;
 }
